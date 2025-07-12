d2d05cf7e7862312e2738b9c4b02f047190fda7b
Pragati Basnet <123282531+PragatiBasnet29@users.noreply.github.com>
2025-07-12 10:57:47 +0545

    Update main.tf

--- main.tf
+++ main.tf
@@ -78,7 +78,14 @@ resource "aws_security_group" "ec2_sg" {
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
   }
-
+ 
+  ingress {
+    description      = "HTTP"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
 
   egress {
     description      = "Allow all outbound"
